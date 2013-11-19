82b69abc2b387b69345888f28aeed839a2e1cc91
Jerry Ablan <jerryablan@dreamfactory.com>
2013-11-19 18:21:09 -0500

    Tuesday work

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
+use DreamFactory\Platform\Utility\ResourceStore;
+use DreamFactory\Platform\Yii\Models\App;
 use DreamFactory\Yii\Utility\Pii;
 use Kisma\Core\Utility\Curl;
+use Kisma\Core\Utility\HtmlMarkup;
+use Kisma\Core\Utility\Inflector;
 
 //	Bootstrap ourselves
 require_once __DIR__ . '/autoload.php';
@@ -14,8 +18,20 @@ if ( Pii::guest() )
 	die();
 }
 
+$_apps = null;
+$_models = ResourceStore::model( 'app' )->findAll( array( 'select' => 'id, api_name', 'order' => 'api_name' ) );
+
+if ( !empty( $_models ) )
+{
+	/** @var App[] $_models */
+	foreach ( $_models as $_model )
+	{
+		$_apps .= HtmlMarkup::tag( 'option', array( 'value' => $_model->id, 'name' => Inflector::neutralize( $_model->api_name ) ), $_model->api_name );
+	}
+}
+
 //	Default url
-$_dspUrl .= '/rest/system/user';
+$_defaultUrl = $_dspUrl . '/rest/system/user';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,13 +40,14 @@ $_dspUrl .= '/rest/system/user';
 	<meta charset="utf-8">
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta charset="utf-8">
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 	<script src="//google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>
 
-	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->    <!--[if lt IE 9]>
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
 	<script src="js/html5shiv.js"></script>
-	<script src="js/respond.min.js"></script>    <![endif]-->
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
 
 	<link rel="icon" href="img/apple-touch-icon.png" type="image/png">
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" type="text/css" media="screen">
@@ -43,165 +60,260 @@ $_dspUrl .= '/rest/system/user';
 <body>
 
 <div id="wrap">
-	<nav class="navbar navbar-default navbar-inverse navbar-fixed-top df-header">
-		<div class="navbar-header">
-			<button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<div class="brand-wrap">
-				<img src="img/logo-32x32.png" alt="" />
+<nav class="navbar navbar-default navbar-inverse navbar-fixed-top df-header">
+	<div class="navbar-header">
+		<button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+		<div class="brand-wrap">
+			<img src="img/logo-32x32.png" alt="" />
 
-				<div class="pull-left">
-					<a href="#" class="navbar-brand df-title">DreamFactory Oasys</a>
-					<br />
-					<small>Example Code</small>
-				</div>
+			<div class="pull-left">
+				<a href="#" class="navbar-brand df-title">DreamFactory Oasys</a>
+				<br />
+				<small>Example Code</small>
 			</div>
 		</div>
-		<div class="collapse navbar-collapse">
-			<ul class="nav navbar-nav">
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown" id="themes">Examples<b class="caret"></b></a>
+	</div>
+	<div class="collapse navbar-collapse">
+		<ul class="nav navbar-nav">
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown" id="themes">Examples<b class="caret"></b></a>
 
-					<ul class="dropdown-menu">
-						<li class="dropdown-header">HTML</li>
-						<li>
-							<a href="#" class="example-code" data-provider="github">GitHub</a>
-						</li>
-						<li>
-							<a href="#" class="example-code" data-provider="facebook">Facebook</a>
-						</li>
-						<li>
-							<a href="#" class="example-code" data-provider="salesforce">Salesforce</a>
-						</li>
-					</ul>
-				</li>
+				<ul class="dropdown-menu">
+					<li class="dropdown-header">HTML</li>
+					<li>
+						<a href="#" class="example-code" data-provider="github">GitHub</a>
+					</li>
+					<li>
+						<a href="#" class="example-code" data-provider="facebook">Facebook</a>
+					</li>
+					<li>
+						<a href="#" class="example-code" data-provider="salesforce">Salesforce</a>
+					</li>
+				</ul>
+			</li>
 
-				<li>
-					<a href="https://www.dreamfactory.com/developers/documentation" target="_blank">Docs</a>
-				</li>
-				<li>
-					<a href="https://www.dreamfactory.com/developers/live_API" target="_blank">API</a>
-				</li>
-				<li>
-					<a href="https://www.dreamfactory.com/developers/faq" target="_blank">FAQs</a>
-				</li>
-				<li>
-					<a href="https://www.dreamfactory.com/developers/support" target="_blank">Support</a>
-				</li>
-				<li>
-					<a href="#" id="app-close" target="_blank">Close</a>
-				</li>
-			</ul>
-		</div>
-	</nav>
+			<li>
+				<a href="https://www.dreamfactory.com/developers/documentation" target="_blank">Docs</a>
+			</li>
+			<li>
+				<a href="https://www.dreamfactory.com/developers/live_API" target="_blank">API</a>
+			</li>
+			<li>
+				<a href="https://www.dreamfactory.com/developers/faq" target="_blank">FAQs</a>
+			</li>
+			<li>
+				<a href="https://www.dreamfactory.com/developers/support" target="_blank">Support</a>
+			</li>
+			<li>
+				<a href="#" id="app-close" target="_blank">Close</a>
+			</li>
+		</ul>
+	</div>
+</nav>
 
-	<div class="container">
+<div class="container">
 
-		<section id="call-settings">
-			<div class="panel-group" id="call-settings-group">
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<h4 class="panel-title">
-							<a data-toggle="collapse" data-parent="#call-settings-group" href="#session-form-body">Call Settings</a>
-						</h4>
-					</div>
-					<div id="session-form-body" class="panel-collapse collapse in">
-						<div class="panel-body">
+	<section id="provider-settings">
+		<div class="panel-group" id="provider-settings-group">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#provider-settings-group" href="#provider-form-body">Providers</a>
+						<span class="pull-right"><button type="button" class="btn btn-primary btn-xs"><i class="fa fa-plus-square"></i>Add...</button></span>
+					</h4>
+				</div>
+				<div id="provider-form-body" class="panel-collapse collapse in">
+					<div class="panel-body">
 
-							<form class="form-horizontal" id="call-settings-form">
-								<div class="form-group">
-									<label for="request-uri" class="col-sm-2 control-label">Resource</label>
+						<form class="form-horizontal" id="provider-settings-form">
+							<div class="form-group">
+								<label for="provider-list" class="col-sm-2 control-label">Providers</label>
 
-									<div class="col-sm-10">
-										<input type="text"
-											   class="form-control"
-											   id="request-uri"
-											   value="<?php echo $_dspUrl; ?>"
-											   placeholder="The request URI (i.e. /system/user)">
+								<div class="col-sm-10">
+									<select id="provider-list"></select>
 
-										<p class="help-block">Either an absolute or relative URL.</p>
-									</div>
+									<input type="text"
+										   class="form-control"
+										   id="request-uri"
+										   value="<?php echo $_defaultUrl; ?>"
+										   placeholder="The request URI (i.e. /system/user)">
+
+									<p class="help-block">Either an absolute or relative URL.</p>
+								</div>
+							</div>
+
+							<div class="form-group row">
+								<label for="request-method" class="col-sm-2 control-label">Method</label>
+
+								<div class="col-sm-4">
+									<select class="form-control" id="request-method">
+										<option value="GET">GET</option>
+										<option value="POST">POST</option>
+										<option value="PUT">PUT</option>
+										<option value="PATCH">PATCH</option>
+										<option value="MERGE">MERGE</option>
+										<option value="DELETE">DELETE</option>
+										<option value="OPTIONS">OPTIONS</option>
+										<option value="COPY">COPY</option>
+									</select>
 								</div>
 
-								<div class="form-group row">
-									<label for="request-method" class="col-sm-2 control-label">Method</label>
+								<label for="request-app" class="col-sm-2 control-label">App/API Key</label>
 
-									<div class="col-sm-4">
-										<select class="form-control" id="request-method">
-											<option value="GET">GET</option>
-											<option value="POST">POST</option>
-											<option value="PUT">PUT</option>
-											<option value="PATCH">PATCH</option>
-											<option value="MERGE">MERGE</option>
-											<option value="DELETE">DELETE</option>
-											<option value="OPTIONS">OPTIONS</option>
-											<option value="COPY">COPY</option>
-										</select>
-									</div>
-
-									<label for="request-app" class="col-sm-2 control-label">App/API Key</label>
-
-									<div class="col-sm-4">
-										<select class="form-control" id="request-app">
+								<div class="col-sm-4">
+									<select class="form-control" id="request-app">
+										<optgroup label="Built-In">
 											<option value="admin">admin</option>
 											<option value="launchpad">launchpad</option>
-										</select>
-									</div>
+										</optgroup>
+										<optgroup label="Available">
+											<?php echo $_apps; ?>
+										</optgroup>
+									</select>
 								</div>
+							</div>
 
-								<div class="form-group">
-									<label for="request-body" class="col-sm-2 control-label">Body</label>
+							<div class="form-group">
+								<label for="request-body" class="col-sm-2 control-label">Body</label>
 
-									<div class="col-sm-10">
-										<textarea id="request-body" rows="2" class="form-control"></textarea>
+								<div class="col-sm-10">
+									<textarea id="request-body" rows="2" class="form-control"></textarea>
 
-										<p class="help-block">Must be valid JSON</p>
-									</div>
+									<p class="help-block">Must be valid JSON</p>
 								</div>
+							</div>
 
-								<div class="multientry" data-attribute="request-headers" data-name="request-headers"></div>
-								<hr />
-								<div class="form-group">
-									<div class="col-sm-offset-2 col-sm-10">
-										<button id="send-request" type="button" class="btn btn-warning">Send Request</button>
-									</div>
+							<div class="multientry" data-attribute="request-headers" data-name="request-headers"></div>
+							<hr />
+							<div class="form-group">
+								<div class="form-buttons">
+									<button id="reset-request" type="button" class="btn btn-danger">Reset</button>
+									<button id="send-request" type="button" class="btn btn-warning">Send Request</button>
 								</div>
+							</div>
 
-							</form>
+						</form>
 
-						</div>
 					</div>
 				</div>
 			</div>
-		</section>
+		</div>
+	</section>
 
-		<section id="provider-results">
-			<div class="panel-group" id="call-results-group">
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<h4 class="panel-title">
-							<a name="provider-results" data-toggle="collapse" data-parent="#call-results-group" href="#call-results-body">Call Results
-							</a>
-						</h4>
-					</div>
-					<div id="call-results-body" class="panel-collapse collapse in">
-						<div class="panel-body">
-							<div id="example-code">No calls made</div>
-						</div>
+	<section id="call-settings">
+		<div class="panel-group" id="call-settings-group">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#call-settings-group" href="#session-form-body">Call Settings</a>
+					</h4>
+				</div>
+				<div id="session-form-body" class="panel-collapse collapse in">
+					<div class="panel-body">
+
+						<form class="form-horizontal" id="call-settings-form">
+							<div class="form-group">
+								<label for="request-uri" class="col-sm-2 control-label">Resource</label>
+
+								<div class="col-sm-10">
+									<input type="text"
+										   class="form-control"
+										   id="request-uri"
+										   value="<?php echo $_defaultUrl; ?>"
+										   placeholder="The request URI (i.e. /system/user)">
+
+									<p class="help-block">Either an absolute or relative URL.</p>
+								</div>
+							</div>
+
+							<div class="form-group row">
+								<label for="request-method" class="col-sm-2 control-label">Method</label>
+
+								<div class="col-sm-4">
+									<select class="form-control" id="request-method">
+										<option value="GET">GET</option>
+										<option value="POST">POST</option>
+										<option value="PUT">PUT</option>
+										<option value="PATCH">PATCH</option>
+										<option value="MERGE">MERGE</option>
+										<option value="DELETE">DELETE</option>
+										<option value="OPTIONS">OPTIONS</option>
+										<option value="COPY">COPY</option>
+									</select>
+								</div>
+
+								<label for="request-app" class="col-sm-2 control-label">App/API Key</label>
+
+								<div class="col-sm-4">
+									<select class="form-control" id="request-app">
+										<optgroup label="Built-In">
+											<option value="admin">admin</option>
+											<option value="launchpad">launchpad</option>
+										</optgroup>
+										<optgroup label="Available">
+											<?php echo $_apps; ?>
+										</optgroup>
+									</select>
+								</div>
+							</div>
+
+							<div class="form-group">
+								<label for="request-body" class="col-sm-2 control-label">Body</label>
+
+								<div class="col-sm-10">
+									<textarea id="request-body" rows="2" class="form-control"></textarea>
+
+									<p class="help-block">Must be valid JSON</p>
+								</div>
+							</div>
+
+							<div class="multientry" data-attribute="request-headers" data-name="request-headers"></div>
+							<hr />
+							<div class="form-group">
+								<div class="form-buttons">
+									<button id="reset-request" type="button" class="btn btn-danger">Reset</button>
+									<button id="send-request" type="button" class="btn btn-warning">Send Request</button>
+								</div>
+							</div>
+
+						</form>
+
 					</div>
 				</div>
 			</div>
-		</section>
+		</div>
+	</section>
 
-	</div>
+	<section id="provider-results">
+		<div class="panel-group" id="call-results-group">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4 class="panel-title">
+						<a name="provider-results" data-toggle="collapse" data-parent="#call-results-group" href="#call-results-body">Call Results</a>
+						<span id="loading-indicator" class="pull-right"><i class="fa fa-spinner"></i></span>
+					</h4>
+				</div>
+				<div id="call-results-body" class="panel-collapse collapse in">
+					<div class="panel-body">
+						<div id="example-code">Waiting...</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+</div>
 </div>
 
 <?php require_once( 'views/_footer.php' ); ?>
-
+<script>
+var _baseUrl = '<?php echo $_dspUrl; ?>';
+</script>
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/jquery.mousewheel.min.js"></script>
 <script type="text/javascript" src="js/mwheelintent.min.js"></script>
--- js/app.jquery.js
+++ js/app.jquery.js
@@ -2,7 +2,6 @@
  * oasys.jquery.js
  * The file contains common client-side functions for the DreamFactory Oasys(tm) Example Code
  */
-
 /**
  * Our global options
  */
@@ -11,7 +10,7 @@ var _options = {
 	notifyDiv:           'div#request-message',
 	ajaxMessageFadeTime: 6000,
 	scrollPane:          null,
-	actions:             window.parent.Actions || null
+	defaultUri:          '/rest/system/user'
 };
 
 /**
@@ -20,12 +19,16 @@ var _options = {
  * @returns {boolean}
  * @private
  */
-var _showResults = function(data) {
+var _showResults = function( data ) {
 	$('#example-code').html('<pre class="prettyprint">' + JSON.stringify(data, null, '\t') + '</pre>');
+
+	//noinspection JSUnresolvedFunction
 	PR.prettyPrint();
+
 	if (-1 == window.location.href.indexOf('#')) {
 		window.location.href += '#provider-results';
 	}
+
 	return true;
 };
 
@@ -35,10 +38,18 @@ var _showResults = function(data) {
  * @returns {*}
  * @private
  */
-var _actions = function(method) {
+var _actions = function( method ) {
 	if (_options.actions && _options.actions[method]) {
-		return _options.actions[method];
+		var _args = [];
+
+		if (arguments.length) {
+			Array.prototype.push.apply(_args, arguments);
+			_args.shift();
+		}
+		return _options.actions[method].apply(null, _args);
 	}
+
+	throw 'Invalid method "' + method + '"';
 };
 
 /**
@@ -50,11 +61,14 @@ var _execute = function() {
 	var _uri = $('#request-uri').val(), _folder = $('#request-x-folder-name').val();
 	var _app = $('#request-x-app-name').val() || 'oasys';
 	var _raw = $('#request-body').val();
+	var $_code = $('#example-code');
 
 	if (!_uri || !_uri.length) {
 		alert('Invalid Request URI specified.');
 		return false;
 	}
+
+	$_code.empty();
 
 	try {
 		var _body = null;
@@ -64,56 +78,81 @@ var _execute = function() {
 		}
 
 		$.ajax({
-			url:         _uri,
-			async:       true,
-			type:        _method,
-			dataType:    'json',
-			cache:       false,
-			processData: false,
-			data:        _body,
-			beforeSend:  function(xhr) {
-				if (_xMethod) {
-					xhr.setRequestHeader('X-HTTP-Method', _xMethod);
-				}
-				if (_folder) {
-					xhr.setRequestHeader('X-Folder-Name', _folder);
-				}
-				if (_app) {
-					xhr.setRequestHeader('X-DreamFactory-Application-Name', _app);
-				}
-			},
-			success:     function(data) {
-				return _showResults(data);
-			},
-			error:       function(err) {
-				if (err.responseText) {
-					return _showResults(JSON.parse(err.responseText));
-				}
-			}
-		});
-	}
-	catch (_ex) {
-		$('#example-code').html(' >> ' + _ex);
+				   url:         _uri,
+				   async:       true,
+				   type:        _method,
+				   dataType:    'json',
+				   cache:       false,
+				   processData: false,
+				   data:        _body,
+				   beforeSend:  function( xhr ) {
+					   $('#loading-indicator').addClass('fa-spin').show();
+					   $('#send-request').addClass('disabled');
+
+					   if (_xMethod) {
+						   xhr.setRequestHeader('X-HTTP-Method', _xMethod);
+					   }
+					   if (_folder) {
+						   xhr.setRequestHeader('X-Folder-Name', _folder);
+					   }
+					   if (_app) {
+						   xhr.setRequestHeader('X-DreamFactory-Application-Name', _app);
+					   }
+				   },
+				   success:     function( data ) {
+					   return _showResults(data);
+				   },
+				   error:       function( err ) {
+					   if (err.responseText) {
+						   _showResults(JSON.parse(err.responseText));
+					   }
+				   },
+				   complete:    function() {
+					   $('#send-request').removeClass('disabled');
+					   $('#loading-indicator').fadeOut().removeClass('fa-spin');
+				   }
+			   });
+	} catch (_ex) {
+		$_code.html(' >> ' + _ex);
 	}
 
 	return false;
 };
 
 /**
+ * Initialize the app
+ * @private
+ */
+var _initialize = function() {
+	if (!_options.actions) {
+		_options.actions = window.parent.Actions;
+		_options.config = window.parent.Config;
+	}
+
+	//	Load providers
+	//	Load apps
+};
+
+/**
  * Initialize any buttons and set fieldset menu classes
  */
-jQuery(function($) {
-	$(window).on('blur', function(e) {
-		_actions('toggleFullScreen')(true);
-	});
+jQuery(function( $ ) {
+	//	Initialize...
+	_initialize();
+
+	$('body').on('open.dsp reopen.dsp',function( e ) {
+		_actions('toggleFullScreen', true);
+	}).on('close.dsp', function() {
+			  _actions('toggleFullScreen', false);
+		  });
 
 	$('.multientry').multientry({
-		label:       'Header(s)',
-		formId:      'call-settings-form',
-		placeholder: 'Header (i.e. &quot;Content-Type: application/json&quot;)',
-	});
+									label:       'Header(s)',
+									formId:      'call-settings-form',
+									placeholder: 'Header (i.e. &quot;Content-Type: application/json&quot;)'
+								});
 
-	$('a.example-code').on('click', function(e) {
+	$('a.example-code').on('click', function( e ) {
 		e.preventDefault();
 		var _which = $(this).data('provider');
 
@@ -123,15 +162,24 @@ jQuery(function($) {
 	});
 
 	//	Close the app
-	$('#app-close').on('click', function(e) {
+	$('#app-close').on('click', function( e ) {
 		e.preventDefault();
-		_actions('toggleFullScreen')(false);
-		_actions('showAdmin')();
+		_actions('showAdmin');
+		_actions('toggleFullScreen', false);
 	});
 
-	$('#send-request').on('click', function(e) {
+	$('#send-request').on('click', function( e ) {
 		e.preventDefault();
 		_execute();
+	});
+
+	$('#reset-request').on('click', function( e ) {
+		e.preventDefault();
+		$('#request-uri').val(_baseUrl + _options.defaultUri);
+		$('#request-method').val('GET');
+		$('#request-app').val('admin');
+		$('#example-code').html('Waiting...');
+		$('#loading-indicator').hide().removeClass('fa-spin');
 	});
 
 	/**
