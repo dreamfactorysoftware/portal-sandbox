bee84b27851318cd2e485bc9a0f324ee532d1bee
Jerry Ablan <jerryablan@dreamfactory.com>
2013-11-20 15:56:44 -0500

    Wednesday afternoon

--- js/app.jquery.js
+++ js/app.jquery.js
@@ -10,7 +10,10 @@ var _options = {
 	notifyDiv:           'div#request-message',
 	ajaxMessageFadeTime: 6000,
 	scrollPane:          null,
-	defaultUri:          '/rest/system/user'
+	defaultUri:          '/rest/system/user',
+	//	Set in index.php
+	providers:           {},
+	baseUrl:             null
 };
 
 /**
@@ -19,7 +22,7 @@ var _options = {
  * @returns {boolean}
  * @private
  */
-var _showResults = function( data ) {
+var _showResults = function(data) {
 	$('#example-code').html('<pre class="prettyprint">' + JSON.stringify(data, null, '\t') + '</pre>');
 
 	//noinspection JSUnresolvedFunction
@@ -38,7 +41,7 @@ var _showResults = function( data ) {
  * @returns {*}
  * @private
  */
-var _actions = function( method ) {
+var _actions = function(method) {
 	if (_options.actions && _options.actions[method]) {
 		var _args = [];
 
@@ -68,7 +71,9 @@ var _execute = function() {
 		return false;
 	}
 
-	$_code.empty();
+	_uri += ( -1 == _uri.indexOf('?') ? '?' : '&') + 'flow_type=1';
+
+	$_code.empty().html('<small>Loading...</small>');
 
 	try {
 		var _body = null;
@@ -78,41 +83,53 @@ var _execute = function() {
 		}
 
 		$.ajax({
-				   url:         _uri,
-				   async:       true,
-				   type:        _method,
-				   dataType:    'json',
-				   cache:       false,
-				   processData: false,
-				   data:        _body,
-				   beforeSend:  function( xhr ) {
-					   $('#loading-indicator').addClass('fa-spin').show();
-					   $('#send-request').addClass('disabled');
+			url:         _uri,
+			async:       true,
+			type:        _method,
+			dataType:    'json',
+			cache:       false,
+			processData: false,
+			data:        _body,
+			beforeSend:  function(xhr) {
+				$('#loading-indicator').addClass('fa-spin').show();
+				$('#send-request').addClass('disabled');
 
-					   if (_xMethod) {
-						   xhr.setRequestHeader('X-HTTP-Method', _xMethod);
-					   }
-					   if (_folder) {
-						   xhr.setRequestHeader('X-Folder-Name', _folder);
-					   }
-					   if (_app) {
-						   xhr.setRequestHeader('X-DreamFactory-Application-Name', _app);
-					   }
-				   },
-				   success:     function( data ) {
-					   return _showResults(data);
-				   },
-				   error:       function( err ) {
-					   if (err.responseText) {
-						   _showResults(JSON.parse(err.responseText));
-					   }
-				   },
-				   complete:    function() {
-					   $('#send-request').removeClass('disabled');
-					   $('#loading-indicator').fadeOut().removeClass('fa-spin');
-				   }
-			   });
-	} catch (_ex) {
+				if (_xMethod) {
+					xhr.setRequestHeader('X-HTTP-Method', _xMethod);
+				}
+				if (_folder) {
+					xhr.setRequestHeader('X-Folder-Name', _folder);
+				}
+				if (_app) {
+					xhr.setRequestHeader('X-DreamFactory-Application-Name', _app);
+				}
+			},
+			success:     function(data) {
+				return _showResults(data);
+			},
+			error:       function(err) {
+				if (302 == err.status || 307 == err.status) {
+					$_code.empty().html('<h4>Authorization Required</h4><p>Please click the link below to authorize this provider.</p><p>' + err.location +
+										'</p>');
+				}
+				else if (err.responseText) {
+					var _json = JSON.parse(err.responseText);
+					if (!_json) {
+						_json = err.responseText;
+					}
+					_showResults(_json);
+				}
+				else {
+					_showResults('Error: ' + err.status);
+				}
+			},
+			complete:    function() {
+				$('#send-request').removeClass('disabled');
+				$('#loading-indicator').fadeOut().removeClass('fa-spin');
+			}
+		});
+	}
+	catch (_ex) {
 		$_code.html(' >> ' + _ex);
 	}
 
@@ -134,25 +151,38 @@ var _initialize = function() {
 };
 
 /**
+ * Load the provider stuff
+ * @param provider
+ * @private
+ */
+var _loadProvider = function(provider) {
+	$('#request-uri').val(_options.baseUrl + '/rest/portal/' + provider.api_name);
+	$('#request-method').val('GET');
+	$('#request-app').val('oasys-examples');
+	$('#example-code').html('<small>Ready</small>');
+	$('#loading-indicator').hide().removeClass('fa-spin');
+};
+
+/**
  * Initialize any buttons and set fieldset menu classes
  */
-jQuery(function( $ ) {
+jQuery(function($) {
 	//	Initialize...
 	_initialize();
 
-	$('body').on('open.dsp reopen.dsp',function( e ) {
+	$('body').on('open.dsp reopen.dsp',function() {
 		_actions('toggleFullScreen', true);
 	}).on('close.dsp', function() {
-			  _actions('toggleFullScreen', false);
-		  });
+			_actions('toggleFullScreen', false);
+		});
 
 	$('.multientry').multientry({
-									label:       'Header(s)',
-									formId:      'call-settings-form',
-									placeholder: 'Header (i.e. &quot;Content-Type: application/json&quot;)'
-								});
+		label:       'Header(s)',
+		formId:      'call-settings-form',
+		placeholder: 'Header (i.e. &quot;Content-Type: application/json&quot;)'
+	});
 
-	$('a.example-code').on('click', function( e ) {
+	$('a.example-code').on('click', function(e) {
 		e.preventDefault();
 		var _which = $(this).data('provider');
 
@@ -162,24 +192,50 @@ jQuery(function( $ ) {
 	});
 
 	//	Close the app
-	$('#app-close').on('click', function( e ) {
+	$('#app-close').on('click', function(e) {
 		e.preventDefault();
-		_actions('showAdmin');
-		_actions('toggleFullScreen', false);
+		if (window.parent && window.parent.Actions) {
+			window.parent.Actions.showAdmin();
+			window.parent.Actions.toggleFullScreen(false);
+		}
 	});
 
-	$('#send-request').on('click', function( e ) {
+	$('#add-provider').on('click', function(e) {
+		e.preventDefault();
+		if (!$(this).hasClass('disabled')) {
+			$('#select-provider').slideUp();
+			$('#new-provider').slideDown();
+			$('#add-provider').addClass('disabled');
+		}
+	});
+
+	$('#add-provider-cancel').on('click', function(e) {
+		e.preventDefault();
+		$('#select-provider').slideDown();
+		$('#new-provider').slideUp();
+		$('#add-provider').removeClass('disabled');
+	});
+
+	$('#send-request').on('click', function(e) {
 		e.preventDefault();
 		_execute();
 	});
 
-	$('#reset-request').on('click', function( e ) {
+	$('#reset-request').on('click', function(e) {
 		e.preventDefault();
-		$('#request-uri').val(_baseUrl + _options.defaultUri);
+		$('#request-uri').val(_options.baseUrl + _options.defaultUri);
 		$('#request-method').val('GET');
 		$('#request-app').val('admin');
-		$('#example-code').html('Waiting...');
+		$('#example-code').html('<small>Ready</small>');
 		$('#loading-indicator').hide().removeClass('fa-spin');
+	});
+
+	$('#provider-list').on('change', function() {
+		var _id = $(this).val();
+
+		if (_options.providers && _options.providers.hasOwnProperty(_id)) {
+			_loadProvider(_options.providers[_id]);
+		}
 	});
 
 	/**
